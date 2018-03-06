2210b3f0f1c1b88bce0d6f668813e88cd9ba37e1
Graeme <graemenorman@tengroup.com>
2018-03-06 17:43:53 +0000

    NEW "selected" deals page / component

---
+++ src/app/components/deals-page/deals-page.smart-component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { NgRedux, select } from '@angular-redux/store';
+import { IAppState } from '../../store/store';
+import { ActivatedRoute } from '@angular/router';
+import { DEAL_SELECTED } from '../../store/actions';
+
+@Component({
+  selector: 'app-deals-page',
+  templateUrl: './deals-page.smart-component.html',
+  styleUrls: ['./deals-page.smart-component.css']
+})
+export class DealsPageSmartComponent implements OnInit {
+
+  deal_id: string;
+
+  constructor(private ngRedux: NgRedux<IAppState>,
+              private _activeRoute: ActivatedRoute) {
+    this.ngRedux.dispatch({
+      type: DEAL_SELECTED,
+      payload: {
+        id: this._activeRoute.snapshot.params[ 'dealId' ]
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.deal_id = this._activeRoute.snapshot.params[ 'dealId' ];
+  }
+
+}
